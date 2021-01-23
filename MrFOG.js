e10d914dbc4d97fef873c80dfa9bcc8e9b3cacb1
shex-rawa <69361347+shex-rawa@users.noreply.github.com>
2021-01-23 23:02:44 +0300

    Add files via upload

---
+++ MrFOG.js
@@ -0,0 +1,210 @@
+const Discord = require("discord.js");
+const MrFOG = new Discord.Client();
+const MrFOG1 = new Discord.Client();
+const MrFOG2 = new Discord.Client();
+const MrFOG3 = new Discord.Client();
+const MrFOG4 = new Discord.Client();
+const MrFOG5 = new Discord.Client();
+const MrFOG6 = new Discord.Client();
+const MrFOG7 = new Discord.Client();
+const MrFOG8 = new Discord.Client();
+const MrFOG9 = new Discord.Client();
+const MrFOG10 = new Discord.Client();
+const MrFOG11 = new Discord.Client();
+const MrFOG12 = new Discord.Client();
+const MrFOG13 = new Discord.Client();
+const MrFOG14 = new Discord.Client();
+const MrFOG15 = new Discord.Client();
+const MrFOG16 = new Discord.Client();
+const MrFOG17 = new Discord.Client();
+const MrFOG18 = new Discord.Client();
+const MrFOG19 = new Discord.Client();
+const MrFOG20 = new Discord.Client();
+
+MrFOG.on("MrFOG", () => {
+  console.log(MrFOG.user.tag); 
+});
+
+MrFOG.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG1.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});
+MrFOG2.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG3.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG4.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG5.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG6.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});
+MrFOG7.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG8.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});
+MrFOG9.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG10.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG11.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG12.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG13.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!help");
+    }, 100);
+  }
+});
+
+
+MrFOG14.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});
+MrFOG15.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG16.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG17.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG18.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG19.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});MrFOG20.on("message", msg => {
+  if (msg.content === ".") {
+    setInterval(() => {
+      msg.channel.send("E!lock");
+    }, 100);
+  }
+});
+
+MrFOG.login("Nzk4MTU4NTIzMTEyMjI2ODI2.X_w9DA.noxr6-LHBRiksaTQVplEKF7q4sw");
+MrFOG1.login("")
+MrFOG2.login("");
+MrFOG3.login("")
+MrFOG4.login("");
+MrFOG5.login("")
+MrFOG6.login("");
+MrFOG7.login("")
+MrFOG8.login("");
+MrFOG9.login("")
+MrFOG10.login("");
+MrFOG11.login("")
+MrFOG12.login("");
+MrFOG13.login("")
+MrFOG14.login("");
+MrFOG15.login("")
+MrFOG16.login("");
+MrFOG17.login("")
+MrFOG18.login("");
+MrFOG19.login("")
+MrFOG20.login("")
